f96dffef2b5c2f1efca85b695e0f537991f700bd
jordanclassof2021 <31080225+jordanclassof2021@users.noreply.github.com>
2017-09-18 19:17:18 -0400

    Initial commit

---
+++ src/helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld {
+ public static void main( String[] args )
+ {
+     System.out.println("Hello, World!");
+     System.out.println("Today is September 13, 2017. I am alive!");
+     System.out.println("My name is Jordan Lewis and I am the best");
+ }
+ }
